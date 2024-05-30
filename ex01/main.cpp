59e025eba9defa35543bee43f7e0ef32f26c90d5
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-05-30 20:09:06 +0200

    fix: print value

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 16:50:47 by jmigoya-          #+#    #+#             */
-/*   Updated: 2024/05/14 16:48:48 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2024/05/30 20:04:36 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,12 @@ int main(void)
 {
 	Data *d1 = new Data();
 	d1->type = "Hola, mundo!";
+	d1->value = 42;
 	uintptr_t serialized = Serializer::serialize(d1);
 	std::cout << "serialized is: " << serialized << std::endl;
 	Data *d2 = Serializer::deserialize(serialized);
-	std::cout << "d2 is type: " << d2->type << std::endl;
+	std::cout << "d2 is type: " << d2->type << "with value:" << d2->value
+			  << std::endl;
 	delete d1;
 	return 0;
 }
