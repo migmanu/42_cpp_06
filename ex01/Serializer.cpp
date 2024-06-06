8c752770934563d6d4b5d78333a0da2adf255f27
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-06-06 17:17:26 +0200

    style(Serializer.cpp): add comments and improve indentation
    Added comments to the serialize and deserialize functions in
    Serializer.cpp to explain their purpose. Also, improved the indentation
    for better readability of the code.

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -6,18 +6,20 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 16:26:12 by jmigoya-          #+#    #+#             */
-/*   Updated: 2024/05/14 16:49:47 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2024/06/06 17:16:06 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 
+// This function serializes a Data pointer into an integer
 uintptr_t Serializer::serialize(Data *ptr)
 {
-	return reinterpret_cast<uintptr_t>(ptr);
+    return reinterpret_cast<uintptr_t>(ptr);
 }
 
+// This function deserializes an integer back into a Data pointer
 Data *Serializer::deserialize(uintptr_t raw)
 {
-	return reinterpret_cast<Data *>(raw);
+    return reinterpret_cast<Data *>(raw);
 }
